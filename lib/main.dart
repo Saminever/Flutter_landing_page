11767880f00e2e7f1884fa716c8df4f8b967937b
MUHAMMAD SAMI <111252345+Saminever@users.noreply.github.com>
2023-09-16 03:46:23 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_web/constants.dart';
+ import 'package:flutter_web/constants.dart';
 import 'package:flutter_web/screens/home/home.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
